78b29caf2c584701e1ccc560d0038929a1f85930
benjamin <benjamin@horsecom.io>
2017-10-22 14:48:18 +0200

    when parsing  external file, remove last empty newline if any

--- mowers.js
+++ mowers.js
@@ -8,6 +8,9 @@ let newFile;
 fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
   if (!err) {
   	newFile = data.split('\n');
+
+  	// Prevent from auto newline insertion
+  	newFile[newFile.length - 1] == "" ? newFile.pop() : null;
   } else {
     console.log(err);
   }
