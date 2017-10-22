71473f97ce5c29a44f4b24a12d914a57aef10eab
benjamin <benjamin@horsecom.io>
2017-10-22 14:57:09 +0200

    create lawn

--- mowers.js
+++ mowers.js
@@ -5,12 +5,19 @@ let fs = require('fs'),
 let newFile;
 
 
+let createLawn = (maxX, maxY) => {
+	return {maxX: maxX, maxY: maxY};
+}
+
 fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
   if (!err) {
   	newFile = data.split('\n');
 
   	// Prevent from auto newline insertion
   	newFile[newFile.length - 1] == "" ? newFile.pop() : null;
+
+  	let lawnPos = newFile.shift().split(' ');
+  	let lawn = createLawn(parseInt(lawnPos[0]), parseInt(lawnPos[1]));
   } else {
     console.log(err);
   }
